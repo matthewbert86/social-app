170d3250ed7ba77486d708ae36bba992d14a873e
Matt <matthewbertweb@gmail.com>
2020-07-21 13:28:39 -0400

    I set up database security. I first installed the npm file dotinstall, then I moved the mongo info to .env - Finally, I required in dotinstall on db.js, and changed the mongo connection function to call in the .env file

--- db.js
+++ db.js
@@ -1,18 +1,21 @@
 // npm install mongodb
+// npm install dotenv
+
+// require in dotenv
+const dotenv = require('dotenv')
+// This will load in all the values in the .env file
+dotenv.config()
 
 // require in mongodb
 const mongodb = require('mongodb')
 
-// This will connect us to our mongodb account and database
-let connectionString =
-  "mongodb://todoAppUser:4L963fFWi74hFQB5@cluster0-shard-00-00-ummw7.mongodb.net:27017,cluster0-shard-00-01-ummw7.mongodb.net:27017,cluster0-shard-00-02-ummw7.mongodb.net:27017/MentalGeek?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-// this will open connection to a mongodb database set up above
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+// this will open connection to a mongodb database set up through our .env file and required in above.
+  // "process.env.CONNECTIONSTRING" is how we access environment variables in node.js
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     // This will return the database object we need to work with
     module.exports = client.db()
     // lets start our express app below
     const app = require('./app')
     // listen for incoming requests
-    app.listen(3000)
+    app.listen(process.env.PORT)
 });
